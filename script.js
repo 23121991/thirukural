6075885cdd5b056d27e2c007a0e0d2dcb90768b6
SARAVANAN R <108886356+23121991@users.noreply.github.com>
2023-05-04 12:47:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
-var div = document.createElement("div")
-div.style.textAlign = "center";
+async function kural(){
+    var number = document.getElementById('kural').value
+     var result = await fetch(`https://api-thirukkural.vercel.app/api?num=${number}`);
+      var datas = await result.json()
+      console.log(datas)
+    var result=document.querySelector('.container-fluid')
+    
+      result.innerHTML=`
+      <div class="section"><h2> ${datas.sect_tam}-${datas.chapgrp_tam}</h2></div>
+      <div class="section"><h4>அதிகாரம்:${datas.chap_tam}🍁</h4></div><br>
+      <div class="section"><h4> குறள்:</h4></div>
+      <div class="section"><h5>${datas.line1}</h5></div>
+      <div class="section"><h5>${datas.line2}</h5></div><br>
+      <div class="section"><h4>பொருள்:</h4></div>
+      <div class="section"><h5>${datas.tam_exp}<h5></div><br><hr>
 
-var input = document.createElement("input");
-input.setAttribute("type", "text");
-input.setAttribute("id", "country");
-
-var button = document.createElement("button");
-button.setAttribute("type", "button");
-button.innerHTML = "Search";
-button.addEventListener("click",foo);
-
-let domains= document.createElement("div");
-domains.setAttribute("Id", "domains");
-let alpha_two_code = document.createElement("alpha_two_code");
-alpha_two_code.setAttribute("Id", "alpha_two_code");
-// let state-province= document.createElement("div");
-// state-province.setAttribute("Id","state-province");
-
-div.append(input,button,domains,alpha_two_code);
-document.body.append(div);
-
-async function foo() {
-
-    let res = document.getElementById("country").value;
-    var url = `http://universities.hipolabs.com/search?country/${res}`;
-
-    let result = await fetch(url);
-    let result1 = await result.json();
-    let index = result1.length-1;
-    console.log(result1[index].domains);
-    domains.innerHTML =`domains:${result1[index].domains}`;
-
-    console.log(result1[index].alpha_two_code);
-    alpha_two_code.innerHTML =`alpha_two_code:${result1[index].alpha_two_code}`;
-
-    // console.log(result1[index].state-province);
-    // state-province.innerHTML =`total state-province:${result1[index].state-province}`;
-
-
-
-
-
-
-
-}
+      <div class="eng"><h2>${datas.sect_eng}-${datas.chapgrp_eng}</h2></div><br>
+      <div class="eng"><h4>Chapter:${datas.chap_eng}🍁<h4></div><br>
+      <div class="eng"><h4>Kural:</h4></div>
+      <div class="eng"> <h5>${datas.eng}</h5></div><br>
+      <div class="eng"><h4>Explanation:</h4></div>
+      <div class="eng"><h5>${datas.eng_exp}</h5></div><br><hr>
+     
+      `
+      
+     }
+  
+  kural()
+  
+  var button = document.querySelector('.btn')
+  button.addEventListener('click',kural)
